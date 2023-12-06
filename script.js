58360cbcdffe593a24aad7ca498875cea92b00e8
19dolly-hub <dollyraghuwanshi16@gmail.com>
2023-12-06 15:17:27 +0530

    update script.js

--- script.js
+++ script.js
@@ -8,110 +8,110 @@ const images = document.querySelectorAll(".page4 .img-container");
 
 // LOADING ANIMATION
 const loadAnimation = () => {
-    const tl = gsap.timeline();
+  const tl = gsap.timeline();
 
-    tl.from("header", {
-        y:-150,
-        duration:0.5
-    });
+  tl.from("header", {
+    y: -150,
+    duration: 0.5,
+  });
 
-    tl.from(".page1 h1", {
-        y: 150,
-        opacity: 0,
-        duration: 0.8,
-        stagger: 0.3
-    });
+  tl.from(".page1 h1", {
+    y: 150,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.3,
+  });
 
-    tl.from(".page1 #container", {
-        scale: 0.9,
-        opacity: 0,
-        duration:0.5
-    })
-}
+  tl.from(".page1 #container", {
+    scale: 0.9,
+    opacity: 0,
+    duration: 0.5,
+  });
+};
 loadAnimation();
 
 // NAVBAR ANIMATION
 const navAnim = () => {
-    gsap.to(".logo svg", {
-        transform: "translateY(-124%)",
-        scrollTrigger: {
-            trigger: ".page1",
-            scroller: "body",
-            start: "top 0",
-            end: "top -8%",
-            scrub: true,
-        }
-    });
+  gsap.to(".logo svg", {
+    transform: "translateY(-124%)",
+    scrollTrigger: {
+      trigger: ".page1",
+      scroller: "body",
+      start: "top 0",
+      end: "top -8%",
+      scrub: true,
+    },
+  });
 
-    gsap.from(".logo svg", {
-        transform: "translateY(-124%)",
-        scrollTrigger: {
-            trigger: ".page6",
-            scroller: "body",
-            start: "top 0",
-            end: "top -8%",
-            scrub: true,
-        }
-    });
+  gsap.from(".logo svg", {
+    transform: "translateY(-124%)",
+    scrollTrigger: {
+      trigger: ".page6",
+      scroller: "body",
+      start: "top 0",
+      end: "top -8%",
+      scrub: true,
+    },
+  });
 
-    gsap.to("nav .links", {
-        transform: "translateY(-100%)",
-        opacity: 0,
-        scrollTrigger: {
-            trigger: ".page1",
-            scroller: "body",
-            start: "top 0",
-            end: "top -10%",
-            scrub: true
-        }
-    })
-}
+  gsap.to("nav .links", {
+    transform: "translateY(-100%)",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".page1",
+      scroller: "body",
+      start: "top 0",
+      end: "top -10%",
+      scrub: true,
+    },
+  });
+};
 navAnim();
 
 // PLAY BUTTON ANIMATION
 const playAnimation = () => {
-    container.addEventListener("mouseenter", function() {
-        gsap.to(play, {
-            transform: "scale(1) translate(-50%, -50%)",
-        });
+  document.addEventListener("mousemove", (det) => {
+    gsap.to(play, {
+      left: det.x,
+      top: det.y,
     });
-    
-    document.addEventListener("mousemove", function(det) {
-        gsap.to(play, {
-            left: det.x,
-            top: det.y
-        });
+  });
+
+  container.addEventListener("mouseenter", function () {
+    gsap.to(play, {
+      transform: "scale(1) translate(-50%, -50%)",
     });
-    
-    container.addEventListener("mouseleave", function() {
-        gsap.to(play, {
-            transform: "scale(0)"
-        });
+  });
+
+  container.addEventListener("mouseleave", function () {
+    gsap.to(play, {
+      transform: "scale(0)",
     });
-}
-if(window.innerWidth > 600) playAnimation();
+  });
+};
+if (window.innerWidth > 600) playAnimation();
 
 // CURSOR ANIMATION
 const moveCursor = () => {
-    document.addEventListener("mousemove", dets => {
-        gsap.to(cursor, {
-            left: dets.x,
-            top: dets.y
-        });
+  document.addEventListener("mousemove", dets => {
+    gsap.to(cursor, {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  images.forEach((img) => {
+    img.addEventListener("mouseenter", function () {
+      gsap.to(cursor, {
+        transform: "scale(1) translate(-50%, -50%)",
+      });
     });
 
-    images.forEach(img => {
-        img.addEventListener("mouseenter", function() {
-            gsap.to(cursor, {
-                transform: "scale(1) translate(-50%, -50%)"
-            })
-        });
-
-        img.addEventListener("mouseleave", function() {
-            gsap.to(cursor, {
-                transform: "scale(0)"
-            })
-        });
+    img.addEventListener("mouseleave", function () {
+      gsap.to(cursor, {
+        transform: "scale(0)",
+      });
     });
-}
+  });
+};
 moveCursor();
